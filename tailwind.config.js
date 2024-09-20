2f2f2f7f768667543d2b400a99fb6b7c854a7bba
chgnw <christophergunawan10@gmail.com>
2024-09-20 15:36:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '480px',   // Small devices like mobile
+        'md': '768px',   // Medium devices like tablets
+        'lg': '1024px',  // Large devices like laptops
+        'xl': '1280px',  // Extra-large devices like desktops
+        '2xl': '1536px', // Ultra-large screens
+      }
+    },
+  },
+  plugins: [],
+}
+
